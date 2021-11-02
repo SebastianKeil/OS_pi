1e484fc77a3627d5c9f866bd4e4c17c70cc9a569
BastiMPB <skeil.april@gmail.com>
2021-11-02 16:27:17 +0100

    wichtig fürs feeling

--- vorgabe0/e0/kernel/start.c
+++ vorgabe0/e0/kernel/start.c
@@ -13,12 +13,18 @@ void increment_counter() {
 void start_kernel(){
 
 	yellow_on();
+	uart_write(10);
+	kprintf("zum beenden: erst str+a, dann x");
+	uart_write(10);
+	kprintf("Hallo ich bin Dr. Kernel, gib eine Zahl ein: ");
+	uart_write(10);
+	
 
 	// Endless counter
 	for (;;) {
 		increment_counter();
 		
-		if(counter > 3){
+		if(counter == 3){
 			kprintf("Da kommt nichts neues!");
 			uart_write(10);
 			
