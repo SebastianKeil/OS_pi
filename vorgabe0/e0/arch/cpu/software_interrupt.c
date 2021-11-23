4032545352a554fa9a6c5f37789a9c43d5dce6ab
jaschawo <knuknikno@googlemail.com>
2021-11-23 17:39:58 +0100

    undefined_asm somewhat done

---
+++ vorgabe0/e0/arch/cpu/software_interrupt.c
@@ -0,0 +1,5 @@
+#include <kernel/kprintf.h>
+
+void software_interrupt(){
+	kprintf("Assunahmenes");
+}
---
+++ vorgabe0/e0/include/arch/cpu/software_interrupt.h
@@ -0,0 +1,6 @@
+#ifndef SOFTWARE_INTERRUPT_H
+#define SOFTWARE_INTERRUPT_H
+
+void software_interrupt();
+
+#endif
