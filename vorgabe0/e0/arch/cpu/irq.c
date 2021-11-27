2a4b4fd77b19dc56c44c9cb82d9e449f573736ad
jaschawo <knuknikno@googlemail.com>
2021-11-28 00:27:47 +0100

    C Handler funktionieren fast

--- vorgabe0/e0/arch/cpu/irq.c
+++ vorgabe0/e0/arch/cpu/irq.c
@@ -3,7 +3,6 @@
 #include <arch/cpu/shared.h>
 #include <arch/bsp/uart.h>
 #include <lib/ringbuffer.h>
-#include <arch/bsp/sys_timer.h>
 
 #define SYS_TIMER 2
 #define UART_INT 25
@@ -14,16 +13,50 @@ unsigned int sys_timer_pending;
 unsigned int uart_pending;
 unsigned char uart_data;
 
+void irq_test(unsigned int regs[13]){
+	kprintf("###########################################################################\n");
+	kprintf(">>> Registerschnappschuss (TEST TEST TEST) <<<\n");
+	kprintf("R0:\t0x%08x\tR8:\t0x%08x\n"
+			"R1:\t0x%08x\tR9:\t0x%08x\n"
+			"R2:\t0x%08x\tR10:\t0x%08x\n"
+			"R3:\t0x%08x\tR11:\t0x%08x\n"
+			"R4:\t0x%08x\tR12:\t0x%08x\n"
+			"R5:\t0x%08x\tSP:\t0x%08x\n"
+			"R6:\t0x%08x\tLR:\t0x%08x\n"
+			"R7:\t0x%08x\tPC:\t0x%08x\n", regs[0], regs[8], regs[1], regs[9], regs[2], regs[10], regs[3], regs[11], regs[4], regs[12], regs[5], regs[13], regs[6], regs[14], regs[7], regs[15]);
+	kprintf("\n");
+}
 
-void irq(){
+void irq(unsigned int regs[35]){
 	kprintf("irq_handler: now checking pending registers..\n");
-	if(print_register_dump){
-		//print register 
+	if(1){ //TODO replaces with print_register_dump
+		kprintf("###########################################################################\n");
+		kprintf(">>> Registerschnappschuss (aktueller Modus) <<<\n");
+		kprintf("R0:\t0x%08x\tR8:\t0x%08x\n"
+				"R1:\t0x%08x\tR9:\t0x%08x\n"
+				"R2:\t0x%08x\tR10:\t0x%08x\n"
+				"R3:\t0x%08x\tR11:\t0x%08x\n"
+				"R4:\t0x%08x\tR12:\t0x%08x\n"
+				"R5:\t0x%08x\tSP:\t0x%08x\n"
+				"R6:\t0x%08x\tLR:\t0x%08x\n"
+				"R7:\t0x%08x\tPC:\t0x%08x\n", regs[19], regs[27], regs[20], regs[28], regs[21], regs[29], regs[22], regs[30], regs[23], regs[31], regs[24], regs[32], regs[25], regs[33], regs[26], regs[34]);
+		kprintf(">>> Aktuelle Statusregister (SPSR des aktuellen Modus) <<<\n");
+		kprintf("CPSR: %08x\n", regs[18]);
+		kprintf("SPSR: %08x\n", regs[17]);
+		kprintf(">>> Aktuelle modusspezifische Register <<<\n");
+		kprintf("\t\tLR\t\tSP\t\tSPSR\n");
+		kprintf("User/System:\t%08x,\t%08x\n", regs[16], regs[15]);
+		kprintf("Supervisor:\t%08x,\t%08x\t%08x\n", regs[14], regs[13], regs[12]);
+		kprintf("Abort:\t\t%08x,\t%08x\t%08x\n", regs[11], regs[10], regs[9]);
+		kprintf("FIQ:\t\t%08x,\t%08x\t%08x\n", regs[8], regs[7], regs[6]);
+		kprintf("IRQ:\t\t%08x,\t%08x\t%08x\n", regs[5], regs[4], regs[3]);
+		kprintf("Undefined:\t%08x,\t%08x\t%08x\n", regs[2], regs[1], regs[0]);
+		kprintf("System angehalten.");
 	}
 	
 	get_pending_status(&sys_timer_pending, &uart_pending);
 	if(uart_pending){
-		kprintf("uart is pending, pushing char to buffer\n");
+		kprintf("uart is pending, push char to buffer\n");
 		uart_data = uart_read();
 		buffer_push(&uart_data, input_buffer);
 		//put uart_port->uart_port->dr & 255 in ringbuffer
@@ -33,6 +66,7 @@ void irq(){
 		kprintf("!%i\n", print_register_dump);
 		reset_sys_timer();
 	}
+	return;
 	//eigentlich RÜCKSPRUNG
 	//while(1);
 }
--- vorgabe0/e0/kernel/start.c
+++ vorgabe0/e0/kernel/start.c
@@ -1,79 +1,99 @@
-#include <arch/bsp/yellow_led.h>
-#include <config.h>
-#include <arch/bsp/uart.h>
-#include <kernel/kprintf.h>
-#include <arch/bsp/interrupt_ctrl.h>
-#include <arch/bsp/sys_timer.h>
-#include <arch/cpu/check_interrupts.h>
-#include <lib/ringbuffer.h>
+.section .init
+
+.global _start
+
+_start:
+	/* Erkennen ob Hypervisor Mode aktiv */
+	mrs r0, cpsr
+	and r0, r0, #0x1F
+	mov r1, #0x1A	//Modebits = 0x1A = Hypervisor Mode
+	cmp r0, r1		//Vergleichen und Statusbits setzen für bedingte Sprünge
+
+	/* Wenn Hypemode erkannt: abschalten (sprung nach _exitHyper)
+	 * wenn nicht erkannt: dann weitermachen (weiterlaufen nach _bsprak)
+	 */
+	beq _exitHyper
+
+	/* Qemu startet immer alle 4 Kerne
+	 * Wir lassen alle anderen Kerne endless loopen
+	 */
+_checkCores:
+	/* Id des Cpu Cores Abfragen */
+	mrc p15, 0, r0, c0, c0, 5
+	/* Falls nicht core 0 Core disablen */
+	tst r0, #3
+	bne _parkCore
+
+/* not modeled in qemu 6.0 */
+_enableAlignCheck:
+	/* SCTLR des cp15 laden */
+	mrc p15, 0, r0, c1, c0, 0
+	/* A-bit (c1[1]) auf 1 setzen */
+	orr r0, r0, #0x2
+	/* neues SCTLR speichern */
+	mcr p15, 0, r0, c1, c0, 0
 
 
-
-
-
-volatile unsigned int counter = 0;
-volatile unsigned char receive_buffer;
-
-void increment_counter() {
-	counter++;
-}
-
-
-void start_kernel(){
-	
-	kprintf("\n\n");
-	kprintf("*****************************************************\n");
-	kprintf("******************** KERNEL SETUP *******************\n");
-	kprintf("*****************************************************\n\n");
-	
-	/* KERNEL SETUP */
-	disable_uart_fifo();
-	set_uart_receive_interrupt(1);
-	//enable_interrupts_timer();
-	initialize_buffer(input_buffer);
-	enable_interrupts_uart();
-	set_timing(5000000);
-	
-	
-	yellow_on();
-	kprintf("\n\n*****************************************************\n");
-	kprintf("********** zum beenden: erst str+a, dann x **********\n");
-	kprintf("********** s for svc ********************************\n");
-	kprintf("********** a for data_abort *************************\n");
-	kprintf("********** u for undefined **************************\n");
-	kprintf("********** p for prefetch_abort *********************\n");
-	kprintf("********** d switches print ON/OFF ******************\n");
-	kprintf("*****************************************************\n\n");
-	kprintf("Hallo ich bin der Kernel, gib eine Ziffer ein: \n\n");
-	kprintf("\n");
-	
-	/*
-	int a = 0xFFFFFFFF;
-	unsigned int b = 0xABCDEFFF;
-
-	kprintf("%p erste \n%p zweite\n", a, b);
-	kprintf("%8p erste \n%8p zweite\n", a, b);
-	*/
-
-
-	// Endless counter
-	for (;;) {
-		increment_counter();
+_bsprak:
+/* 	MEMORY LAYOUT
+	sp_svc 0x20000 #131072
+	sp_und 0x21000   +4096
+	sp_usr 0x22000   +4096 
+	sp_abt 0x24000   +8192
+	sp_irq 0x25000   +4096
+	sp_fiq 0x26000   +4096*/
 		
-		if(input_buffer->count > 0){
-			input_buffer->count = 0;
-			receive_buffer = buffer_pull(input_buffer);
-			check_for_interrupts(receive_buffer);
-		}
-		// HA 1:
-		//kprintf("Es wurde folgender Charakter eingegeben: %c, In Hexadezimal: %x, In Dezimal: %08i\n", receive_buffer, receive_buffer, receive_buffer);
-		
-		/* generating test interrupts:
-		receive_buffer = uart_read();
-		check_for_interrupts(receive_buffer);
-		*/
+	ldr 	sp,	=0x20000
+	ldr		r0, =0x21000
+	msr 	sp_und, r0
+	/* user mode gets more space*/
+	ldr		r0, =0x22000
+	msr 	sp_usr, r0 
+	ldr		r0, =0x24000
+	msr 	sp_abt, r0 
+	ldr		r0, =0x25000
+	msr 	sp_irq, r0
+	ldr		r0, =0x26000
+	msr 	sp_fiq, r0
 
-	}
+	/* set VBAR in CP15 */
+	ldr r0, =_ivt
+	mcr p15, 0, r0, c12, c0, 0
 	
+	/* change to user mode */
+	//cps #0x10	//nicht in den usermode!
+	cpsie i 	//erlaubt interrupts!!!
 	
-}
+	/* Zu c Code springen */
+	bl  start_kernel
+.Lend:
+	WFI
+	b .Lend
+
+_parkCore:
+	/* Interrupts für Core 1 bis 3 ausschalten  */
+	cpsid if
+	/* In Endlosschleife springen */
+	b .Lend
+
+_exitHyper:
+
+	/* Rücksprungadresse ins Hypervisor Rücksprungregister schreiben.
+	 * Dies ist das Label _bsprak für die normale Programmausfürung.
+	 */
+	ldr lr, =_checkCores
+	msr ELR_hyp, lr
+
+	/* Der Hypervisor mode ist auch "nur" eine etwas kompliziertere Exception.
+	 * Also springen wir aus dieser zurück.
+	 * (Rücksprunge in Aufgabe 2 sind anders zu lösen!).
+	 * Dazu zunächst das CPSR auslesen und die Modebits zum Supervisor ändern.
+	 * Danach dies in das SPSR für den Rücksprung schreiben.
+	 */
+	mrs r0, cpsr
+	bic r0, r0, #0x1F
+	orr r0, r0, #0x13	//Supervisormode
+	msr spsr_hyp, r0
+	eret
+
+#undef __ASSEMBLY__
