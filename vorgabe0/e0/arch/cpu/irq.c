dc11fa226e06aa2230e2e27c76abacf330134454
jaschawo <knuknikno@googlemail.com>
2021-11-28 15:49:05 +0100

    library added ohne timer

--- vorgabe0/e0/arch/cpu/irq.c
+++ vorgabe0/e0/arch/cpu/irq.c
@@ -4,6 +4,7 @@
 #include <arch/bsp/uart.h>
 #include <lib/ringbuffer.h>
 #include <arch/bsp/sys_timer.h>
+#include <lib/utilities.h>
 
 #define SYS_TIMER 2
 #define UART_INT 25
@@ -13,32 +14,6 @@
 unsigned int sys_timer_pending;
 unsigned int uart_pending;
 unsigned char uart_data;
-
-
-void print_reg_dump(unsigned int regs[]){				
-	kprintf("###########################################################################\n");
-		kprintf(">>> Registerschnappschuss (aktueller Modus) <<<\n");
-		kprintf("R0:\t0x%08x\tR8:\t0x%08x\n"
-				"R1:\t0x%08x\tR9:\t0x%08x\n"
-				"R2:\t0x%08x\tR10:\t0x%08x\n"
-				"R3:\t0x%08x\tR11:\t0x%08x\n"
-				"R4:\t0x%08x\tR12:\t0x%08x\n"
-				"R5:\t0x%08x\tSP:\t0x%08x\n"
-				"R6:\t0x%08x\tLR:\t0x%08x\n"
-				"R7:\t0x%08x\tPC:\t0x%08x\n", regs[19], regs[27], regs[20], regs[28], regs[21], regs[29], regs[22], regs[30], regs[23], regs[31], regs[24], regs[32], regs[25], regs[33], regs[26], regs[34]);
-		kprintf(">>> Aktuelle Statusregister (SPSR des aktuellen Modus) <<<\n");
-		kprintf("CPSR: %08x\n", regs[18]);
-		kprintf("SPSR: %08x\n", regs[17]);
-		kprintf(">>> Aktuelle modusspezifische Register <<<\n");
-		kprintf("\t\tLR\t\tSP\t\tSPSR\n");
-		kprintf("User/System:\t%08x,\t%08x\n", regs[16], regs[15]);
-		kprintf("Supervisor:\t%08x,\t%08x\t%08x\n", regs[14], regs[13], regs[12]);
-		kprintf("Abort:\t\t%08x,\t%08x\t%08x\n", regs[11], regs[10], regs[9]);
-		kprintf("FIQ:\t\t%08x,\t%08x\t%08x\n", regs[8], regs[7], regs[6]);
-		kprintf("IRQ:\t\t%08x,\t%08x\t%08x\n", regs[5], regs[4], regs[3]);
-		kprintf("Undefined:\t%08x,\t%08x\t%08x\n", regs[2], regs[1], regs[0]);
-		kprintf("System angehalten.\n");
-}
 
 void irq(unsigned int regs[35]){
 	kprintf("\nirq_handler: there is an irq interrupt!\n");
@@ -60,18 +35,4 @@ void irq(unsigned int regs[35]){
 	return;
 	//eigentlich RÜCKSPRUNG
 	//while(1);
-}
-
-void irq_test(unsigned int regs[13]){
-	kprintf("###########################################################################\n");
-	kprintf(">>> Registerschnappschuss (TEST TEST TEST) <<<\n");
-	kprintf("R0:\t0x%08x\tR8:\t0x%08x\n"
-			"R1:\t0x%08x\tR9:\t0x%08x\n"
-			"R2:\t0x%08x\tR10:\t0x%08x\n"
-			"R3:\t0x%08x\tR11:\t0x%08x\n"
-			"R4:\t0x%08x\tR12:\t0x%08x\n"
-			"R5:\t0x%08x\tSP:\t0x%08x\n"
-			"R6:\t0x%08x\tLR:\t0x%08x\n"
-			"R7:\t0x%08x\tPC:\t0x%08x\n", regs[0], regs[8], regs[1], regs[9], regs[2], regs[10], regs[3], regs[11], regs[4], regs[12], regs[5], regs[13], regs[6], regs[14], regs[7], regs[15]);
-	kprintf("\n");
 }
