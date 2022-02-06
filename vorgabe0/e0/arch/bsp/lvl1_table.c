c90a243434f302a025f0464b7bda180cc3e821f1
jaschawo <knuknikno@googlemail.com>
2022-02-06 16:23:44 +0100

    getting closer

--- vorgabe0/e0/arch/bsp/lvl1_table.c
+++ vorgabe0/e0/arch/bsp/lvl1_table.c
@@ -72,7 +72,7 @@ void initialize_mmu(){
 		//USR_RODATA	//Sys L   	Usr L 	XN
 		else if(i < 7){lvl1_table[i] = set_bits(temp, 1, 1, 1, 1, 0, 1);}	
 		//USR_DATA/BSS 	//Sys L/S 	USR L/S XN
-		else if(i < 8){lvl1_table[i] = set_bits(temp, 1, 1, 0, 1, 0, 1);}	
+		else if(i < 9){lvl1_table[i] = set_bits(temp, 1, 1, 0, 1, 0, 1);}	
 		//HARDWARE		//Sys L/S 	Usr 	XN	
 		else if(i > 1008 && i < 1012){lvl1_table[i] = set_bits(temp, 1, 0, 0, 1, 0, 1);}
 		
@@ -80,7 +80,7 @@ void initialize_mmu(){
 		//HIER IST NOCH EIN FEHLER
 		//FAULT			//Sys 		Usr 	
 		else{lvl1_table[i] = set_bits(temp, 0, 0, 0, 0, 0, 0);
-		//lvl1_table[i] = set_bits(temp, 1, 1, 0, 0, 0, 1);
+		lvl1_table[i] = set_bits(temp, 1, 1, 0, 0, 0, 1);
 	}				 	
 
 		//Vollzugriff
--- vorgabe0/e0/user/unterprogramm.c
+++ vorgabe0/e0/user/unterprogramm.c
@@ -6,6 +6,7 @@
 #define LOWER_BOUND_UPPER_CASE 65
 #define UPPER_BOUND_UPPER_CASE 90
 #define LINEFEED (unsigned char) 10
+#define K_ASCII 75
 
 unsigned char char_for_unterprogramm;
 unsigned int buffer_count;
@@ -40,7 +41,14 @@ void unterprogramm(unsigned char *input){
 	unsigned int range = (unsigned int)character;
 
 	if (range >= LOWER_BOUND_UPPER_CASE && range <= UPPER_BOUND_UPPER_CASE){
-		//kprintf("\tgoing to pint uppercase");
+		if(range == K_ASCII){
+			//lesender Zugriff auf Kernel-Stack
+			asm volatile 	("ldr r0, =0x3FFFF8\t\n"
+							"ldr r0, [r0]\t\n"
+							: : : "r0");
+			syscall_kill_thread();	
+			return;
+		}
 		print_answer_uppercase(input);
 		syscall_kill_thread();	
 		return;
@@ -77,55 +85,50 @@ void unterprogramm(unsigned char *input){
 		syscall_put_char('d');
 	}
 	switch(character){
-		case 'n':
-			//lesender Zugriff auf Null-Pointer
+		case 'n':			//lesender Zugriff auf Null-Pointer
 			asm volatile 	("ldr r0, =0x0\t\n"
 							"ldr r0, [r0]\t\n"
 							: : : "r0");
 			break;
-		case 'p':
-			//Sprung auf Null-Pointer
-			asm volatile ("ldr pc, =0x0");
+
+		case 'p': 			//Sprung auf Null-Pointer
+			asm volatile 	("ldr pc, =0x0");
 			break;
-		case 'd':
-			//lesender Zugriff auf Kernel-Daten
+
+		case 'd':			//lesender Zugriff auf Kernel-Daten
 			asm volatile 	("ldr r0, =0x300000\t\n"
 							"ldr r0, [r0]\t\n"
 							: : : "r0");
 			break;
-		case 'k':
-			//lesender Zugriff auf Kernel-Code
+
+		case 'k':			//lesender Zugriff auf Kernel-Code
 			asm volatile 	("ldr r0, =0x100000\t\n"
 							"ldr r0, [r0]\t\n"
 							: : : "r0");
-			break;
-		case 'K':
-			//lesender Zugriff auf Kernel-Stack
-			asm volatile 	("ldr r0, =0x3FFFF8\t\n"
-							"ldr r0, [r0]\t\n"
-							: : : "r0");
-			break;
-		case 'g':
-			//lesender Zugriff auf Peripherie-Gerät, z.B. UART
+			break;			
+
+		case 'g':			//lesender Zugriff auf Peripherie-Gerät, z.B. UART
 			asm volatile 	("ldr r0, =0x3f201000\t\n"
 							"ldr r0, [r0]\t\n"
 							: : : "r0");
 			break;
-		case 'c':
-			//schreibender Zugriff auf eigenen Code
 
+		case 'c':			//schreibender Zugriff auf eigenen Code
+			
 			break;
-		case 's':
-			//Stack-Overflow
+
+		case 's':			//Stack-Overflow
+			
 			break;
-		case 'u':
-			//lesender Zugriff auf nicht zugeordnete Adresse
+
+		case 'u':			//lesender Zugriff auf nicht zugeordnete Adresse
 			asm volatile 	("ldr r0, =0x5a00000\t\n"
 							"ldr r0, [r0]\t\n"
 							: : : "r0");
 			break;
-		case 'x':
-			//Sprung auf eigene Daten oder Stack
+
+		case 'x':			//Sprung auf eigene Daten oder Stack
+			
 			break;
 		default:
 			print_answer(input);
